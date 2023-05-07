fbbd29649b7a92dbacd1b08f2574e7ed2d2e4bf0
kistain <kyudytskiy@gmail.com>
2023-05-07 17:57:18 +0300

    finalfinal :)

--- src/bismarkplot/__init__.py
+++ src/bismarkplot/__init__.py
@@ -1,3 +1,10 @@
-from BismarkFiles import BismarkFiles, Bismark, BarPlot, BoxPlot, LinePlot, HeatMap
-from read_bismark import read_bismark_batches
-from read_genome import read_genome
+from .BismarkFiles import BismarkFiles
+from .Bismark import Bismark
+from .LinePlot import LinePlot
+from .HeatMap import HeatMap
+from .BarPlot import BarPlot
+from .BoxPlot import BoxPlot
+from .read_bismark import read_bismark_batches
+from .read_genome import read_genome
+
+__all__ = ["BismarkFiles", "Bismark", "BarPlot", "BoxPlot", "LinePlot", "HeatMap", "read_bismark_batches", "read_genome"]
