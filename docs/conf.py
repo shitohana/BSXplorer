b18ce133b1691fd4888dd9bd2f3fe24b01e62f6d
kistain <kyudytskiy@gmail.com>
2023-05-08 00:58:26 +0300

    initial release

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 project = 'BismarkPlot'
 copyright = '2023, shitohana'
 author = 'shitohana'
-release = '0.2.5'
+release = '1.0'
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../src'))
