22b982cb733552dc6780fcebc8507baa69a4cbf7
shitohana <kyudytskiy@gmail.com>
2024-02-05 20:48:28 +0300

    v1.0.0a3

--- src/__init__.py
+++ src/__init__.py
@@ -4,4 +4,4 @@ from .bsxplorer.Binom import BinomialData, RegionStat
 from .bsxplorer.GenomeClass import Genome
 from .bsxplorer.ChrLevels import ChrLevels
 
-__version__ = '1.0.0a2'
+__version__ = '1.0.0a3'
