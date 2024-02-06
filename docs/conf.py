11d4453ae466a7093316a0b64c56bc2f2158f81a
shitohana <kyudytskiy@gmail.com>
2024-02-06 15:53:53 +0300

    1.0.0a5

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@ import sys
 project = 'BSXplorer'
 copyright = '2023, shitohana'
 author = 'shitohana'
-release = '1.0.0a4'
+release = '1.0.0a5'
 sys.path.insert(0, os.path.abspath('../src'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.append(os.path.abspath('.'))
