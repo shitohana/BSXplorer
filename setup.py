ac716125b2dbc3596a7d9f315520c412aab90410
shitohana <kyudytskiy@gmail.com>
2024-12-13 11:32:39 +0300

    fix

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 from Cython.Compiler import Options
 import sys
-import numpy
-
-numpy_include_dir = numpy.get_include()
 
 Options.docstrings = True
 Options.annotate = False
@@ -21,7 +18,6 @@ ext = Extension(
     extra_compile_args=["-std=c++17", openmp_arg],
     extra_link_args=["-lgomp", openmp_arg],
 )
-ext.include_dirs.append(numpy_include_dir)
 
 setup(
     ext_modules=cythonize(ext)
