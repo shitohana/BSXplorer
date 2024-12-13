0abff32a781226090a9857e4f7344b3c10d6f24e
shitohana <kyudytskiy@gmail.com>
2024-12-13 13:11:51 +0300

    idk

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 from Cython.Compiler import Options
 import sys
+import numpy
 
 Options.docstrings = True
 Options.annotate = False
+numpy_path = numpy.get_include()
 
 if sys.platform.startswith("win"):
     openmp_arg = '/openmp'
@@ -18,6 +20,7 @@ ext = Extension(
     extra_compile_args=["-std=c++17", openmp_arg],
     extra_link_args=["-lgomp", openmp_arg],
 )
+ext.include_dirs.append(numpy_path)
 
 setup(
     ext_modules=cythonize(ext)
